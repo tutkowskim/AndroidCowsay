87b948dfb1c477598290afc93cef30234f9a4250
Mark Tutkowski <tutkows1@gmail.com>
2018-09-08 22:23:22 -0500

    Cleanup unused imports.

--- app/src/main/java/cowsay/andriod/tutkowskim/androidcowsay/MainActivity.java
+++ app/src/main/java/cowsay/andriod/tutkowskim/androidcowsay/MainActivity.java
@@ -1,7 +1,5 @@
 package cowsay.andriod.tutkowskim.androidcowsay;
 
-import android.content.ClipData;
-import android.content.ClipboardManager;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -13,9 +11,7 @@ import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
